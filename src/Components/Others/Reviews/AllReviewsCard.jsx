115b412a27f50698257efb59ff7ddff26fac1470
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-12-27 12:54:26 +0600

    resolved review card

--- src/Components/Others/Reviews/AllReviewsCard.jsx
+++ src/Components/Others/Reviews/AllReviewsCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaRegStar, FaStar } from 'react-icons/fa';
+import { FaBook, FaRegStar, FaStar } from 'react-icons/fa';
 import Rating from 'react-rating';
 
 const AllReviewsCard = ({ review }) => {
@@ -35,11 +35,17 @@ const AllReviewsCard = ({ review }) => {
         </svg>
       </div>
       <div className="w-full h-full">
+        <div className="flex justify-around items-center gap-2">
         <img src={data?.image} alt="" className="w-12 h-12 rounded-full mx-auto" />
+        <img src={review?.product_image} alt="" className="w-12 h-12 rounded-full mx-auto" />
+        </div>
         <div className="pt-5 space-y-3">
-          <p className='h-28'>
-            {data?.review?.slice(0,150)}
+          <p className='h-20'>
+            {data?.review?.slice(0, 150)}
           </p>
+          <div className="mt-5">
+            <p className="font-medium flex items-center justify-center gap-2"> <FaBook/> {review?.product_name}</p>
+          </div>
           <div className="flex items-center justify-center gap-2">
             <Rating
               emptySymbol={<FaRegStar className="text-yellow-400" />}
